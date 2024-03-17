6646e6caeec89804be8ae99b1c805ee45e57c588
Ansh-1904 <solankiansh3693@gmail.com>
2024-03-17 12:51:26 +0530

    first commit

--- week-2/express-class/index1.js
+++ week-2/express-class/index1.js
@@ -32,13 +32,86 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
-    const isHealthy=req.body.isHealthy
+    const isHealthy=req.body.isHealthy;
+    console.log("Received POST request with isHealthy value:", isHealthy);
+
     users[0].kidneys.push({
-        healthy:isHealthy
-    })
+        healthy: isHealthy
+    });
+
+    console.log("Updated kidneys array:", users[0].kidneys);
+
     res.json({
-        msg:"Done"
-    })
+        msg: "Done"
+    });
+});
+
+
+app.put("/",function(req,res){
+    let anyUnhealthy = false;
+
+    // Check if any kidneys are unhealthy
+    for(let i = 0; i < users[0].kidneys.length; i++) {
+        if (!users[0].kidneys[i].healthy) {
+            anyUnhealthy = true;
+            break;
+        }
+    }
+
+    // If no unhealthy kidneys, send appropriate response
+    if (!anyUnhealthy) {
+        return res.status(400).json({ error: "All kidneys are already healthy." });
+    }
+
+    // If there are unhealthy kidneys, make them healthy
+    for(let i = 0; i < users[0].kidneys.length; i++) {
+        users[0].kidneys[i].healthy = true;
+    }
+
+    res.json({ message: "All kidneys are now healthy." });
+});
+
+
+app.delete("/",function(req,res){
+
+    if(checkForNumberOfBadKidneys())
+    {
+        const newkidneys=[]
+        for(let i=0;i<users[0].kidneys.length;i++)
+        {
+            if(users[0].kidneys[i].healthy)
+            {
+                newkidneys.push({
+                    healthy:true
+                })
+            }
+
+        }
+        users[0].kidneys=newkidneys
+        res.json({
+            msg: "Done"
+        });
+    }
+    else{
+        res.status(411).json({
+            msg: "you have no bad kidneys"
+        });
+    }
+    
 })
+
+function checkForNumberOfBadKidneys()
+{
+    let atleatOneBadKidney=false;
+    for(let i=0;i<users[0].kidneys.length;i++)
+    {
+        if(!users[0].kidneys[i].healthy)
+        {
+            atleatOneBadKidney=true
+        }
+
+    }
+    return atleatOneBadKidney
+}
 
 app.listen(3000)
---
+++ week-2/week-2.6/index.js
@@ -0,0 +1,9 @@
+const input=[1,2,3,4,5];
+
+function tranform(i)
+{
+    return i*2;
+}
+
+const ans=input.map(tranform);
+console.log(ans);
