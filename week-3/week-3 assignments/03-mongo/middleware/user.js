2b27682cae9928be72c33a4b53457bdb79d8d5f2
Ansh-1904 <solankiansh3693@gmail.com>
2024-04-17 13:01:04 +0530

    asa

--- week-3/week-3 assignments/03-mongo/middleware/user.js
+++ week-3/week-3 assignments/03-mongo/middleware/user.js
@@ -4,8 +4,8 @@ function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB.
     //Check readme for the exact headers to be expected
-    const username=re.header.username;
-    const password=re.header.password;
+    const username=req.header.username;
+    const password=req.header.password;
     User.findOne({
         username:username,
         password:password
--- week-3/week-3 assignments/03-mongo/routes/user.js
+++ week-3/week-3 assignments/03-mongo/routes/user.js
@@ -1,18 +1,47 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course}=require("../db")
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password
+
+    await User.create({
+        username,
+        password
+    })
+    res.json({
+        message:"User created successfully."
+    })
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async(req, res) => {
     // Implement listing all courses logic
+    const response=await Course.find({})
+    res.json({
+        courses:response
+    })
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+router.post('/courses/:courseId', userMiddleware, async(req, res) => {
     // Implement course purchase logic
+    const courseId=req.params.courseId
+    const username=req.headers.username
+    
+    await User.updateOne({
+        username:username
+    },{
+        "$push":{
+            purchasedCourses:courseId
+        }
+    })
+
+    res.json({
+        message:"Purchased complete"
+    })
 });
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
