0557a14eae9ee5f27eef3ca7f79f781ac90c9580
Ansh-1904 <solankiansh3693@gmail.com>
2024-04-21 16:35:18 +0530

    half done with 04-mongo-with-jwt-auth

---
+++ week-3/week-3 assignments/04-mongo-with-jwt-auth/config.js
@@ -0,0 +1,3 @@
+module.exports={
+    JWT_SECRET:"Ansh_server"
+}
--- week-3/week-3 assignments/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/week-3 assignments/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,25 @@
+const jwt=require("jsonwebtoken")
+const {JWT_SECRET}=require("../config")
+
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const token=req.headers.authorization 
+    // Bearer asnjadjnsd
+    const words=token.split(" ")
+    const jwtToken=words[1]
+    const decodedValue=jwt.verify(jwtToken,JWT_SECRET)
+    if(decodedValue.username)
+    {
+        next();
+    }
+    else{
+        res.json({
+            "msg":"You are not authenticated"
+        })
+    }
 }
 
 module.exports = userMiddleware;
